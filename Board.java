0a4dcf28486132c084ed58468f431036b9ea9a4a
Kahlin Baughman <baughmankahlin@gmail.com>
2016-05-03 23:28:13 -1000

    Revised minesweeper

---
+++ Board.java
@@ -0,0 +1,296 @@
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+public class Board {
+	private Cell[][] cells;
+	private JFrame frame;
+	private JPanel easyPanel;
+	private int side ;
+	private int numMines;
+	private JPanel menu;
+	private int numOfFlags;
+	private int nonMines;
+
+
+	Board(){
+		frame = new JFrame("Minesweeper");
+		frame.setLayout(new BorderLayout());
+		frame.setSize(200,200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menu = Menu();
+		frame.add(menu, BorderLayout.CENTER);
+		frame.setVisible(true);
+	}
+
+
+	public JPanel Menu(){
+		JPanel panel = new JPanel();
+		JButton easy = new JButton("Easy");
+		JButton normal = new JButton("Normal");
+		JButton hard = new JButton("Hard");
+		JButton score = new JButton("High Scores");
+		time = new Timer();
+
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		panel.setLayout(new GridBagLayout());
+
+
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		panel.add(easy, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		panel.add(normal, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		panel.add(hard, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		panel.add(score, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		panel.add(time, gbc);
+
+		easy.addActionListener(new easyMode());
+		normal.addActionListener(new normalMode());
+		hard.addActionListener(new hardMode());
+		score.addActionListener(new scoreBoard());
+		return panel;
+	}
+	public JPanel addCells(int side){
+		JPanel panel = new JPanel(new GridLayout(side,side));
+		cells = new Cell[side][side];
+		for(int i = 0; i < side; i++){
+			for(int j = 0; j < side; j++){
+				cells[i][j] = new Cell();
+				cells[i][j].setId(i,j);
+				cells[i][j].addMouseListener(new mouseClicking());
+				panel.add(cells[i][j]);
+			}
+		}
+		this.plantMines(side);
+		this.setCellValues();
+		nonMines = side*side - numMines;
+		return panel;
+	}
+	public void plantMines(int sides){
+		Random random = new Random();
+		int counter = 0;
+		while(counter != numMines){
+			counter += cells[random.nextInt(sides)][random.nextInt(sides)].setMine();
+		}
+	}
+	private IntStream sidesOf(int value) {
+	    return IntStream.rangeClosed(value - 1, value + 1).filter(
+	            x -> x >= 0 && x < side);
+	}
+	private Set<Cell> getSurroundingCells(int x, int y) {
+	    Set<Cell> result = new HashSet<>();
+	    sidesOf(x).forEach(a -> {
+	        sidesOf(y).forEach(b -> result.add(cells[a][b]));
+	    });
+	    result.remove(cells[x][y]);
+	    return result;
+	}
+	private void setCellValues() {
+		for(int i = 0; i < side; i++){
+	    	for(int j = 0; j < side; j++){
+	    		if(cells[i][j].isMine() == true){
+	    			Set<Cell> surrounding = getSurroundingCells(i,j);
+	    			Iterator<Cell> iter = surrounding.iterator();
+	    			while(iter.hasNext()){
+	    				Cell temp = iter.next();
+	    				if(temp.isMine() == false){
+	    					int x = temp.getI();
+	    					int y = temp.getJ();
+	    					cells[x][y].increment();
+	    				}
+	    			}
+	    		}
+	    	}
+	    }
+	}
+	private void scanForEmptyCells(Cell cell){
+		int x = cell.getI();
+		int y = cell.getJ();
+		Set<Cell> surrounding = getSurroundingCells(x,y);
+		Iterator<Cell> iter = surrounding.iterator();
+		while(iter.hasNext()){
+			Cell temp = iter.next();
+			if(temp.isEmpty() == true && temp.isChecked() == false){
+				temp.reveal();
+				nonMines--;
+				scanForEmptyCells(temp);
+			}
+			else{
+				temp.reveal();
+				nonMines--;
+			}
+		}
+	}
+	private class mouseClicking implements MouseListener{
+		boolean pressed;
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			System.out.println("Cristian");
+			//if(){
+
+			//}
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			pressed = true;
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			pressed = false;
+
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			Cell temp = (Cell) e.getSource();
+			temp.getModel().setArmed(true);
+			temp.getModel().setPressed(true);
+			pressed = true;
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			Cell temp = (Cell) e.getSource();
+			temp.getModel().setArmed(false);
+			temp.getModel().setPressed(false);
+			if(pressed){
+				if (SwingUtilities.isRightMouseButton(e)) {
+					if(temp.isFlagged() == false){
+						temp.setText("F");
+						temp.setFlag(true);
+					}
+					else if(temp.isFlagged() == true){
+						temp.setFlag(false);
+						temp.setText(" ");
+					}
+
+                }
+				else if(SwingUtilities.isLeftMouseButton(e)){
+					if(temp.checkMine() == true && temp.isFlagged() == false){
+						gameOver();
+					}
+					else if(temp.isEmpty() == true && temp.isFlagged() == false){
+						scanForEmptyCells(temp);
+						if(nonMines == 0){
+							win();
+						}
+					}
+					else if(temp.isFlagged() == false){
+						temp.reveal();
+						nonMines--;
+						if(nonMines == 0){
+							win();
+						}
+					}
+				}
+			}
+			pressed = false;
+		}
+
+	}
+	private class easyMode implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			frame.remove(menu);
+			SwingUtilities.updateComponentTreeUI(frame);
+			numMines = 9;
+			side = 10;
+			frame.add(addCells(10), BorderLayout.CENTER);
+			JButton test = new JButton();
+			test.setText("Cristain loves loli");
+			frame.add(test, BorderLayout.NORTH);
+		}
+
+	}
+	public void gameOver(){
+		this.revealBoard();
+	}
+	public void revealBoard(){
+		for(int i = 0; i < side ; i++){
+			for(int j = 0; j < side; j++){
+				if(cells[i][j].isChecked() != true){
+					cells[i][j].reveal();
+				}
+			}
+		}
+	}
+	private class normalMode implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			frame.remove(menu);
+			numMines = 50;
+			side = 15;
+			frame.add(addCells(15));
+			SwingUtilities.updateComponentTreeUI(frame);
+		}
+
+	}
+	private class hardMode implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			frame.remove(menu);
+			numMines = 100;
+			side = 20;
+			frame.add(addCells(20));
+			frame.setSize(800,800);
+			SwingUtilities.updateComponentTreeUI(frame);
+		}
+
+	}
+
+	private class scoreBoard implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			frame.remove(menu);
+			numMines = 100;
+			side = 20;
+			frame.add(addCells(20));
+			frame.setSize(800,800);
+			SwingUtilities.updateComponentTreeUI(frame);
+		}
+
+	}
+
+	public void win(){
+		this.revealBoard();
+	}
+}
--- Cell.java
+++ Cell.java
@@ -1,342 +1,81 @@
-/***************************************************************************************
- *
- * NAME: Alyssa Higuchi
- *
- * HOMEWORK: 6
- *
- * CLASS: ICS 211
- *
- * INSTRUCTOR: Scott Robertson
- *
- * DATE: February 25, 2016
- *
- * FILE: Cell.java
- *
- * DESCRIPTION: This file contains the Cell class for homework 6. You can think of a
- *              Cell in the same way that you think of a node.
- *
- ***************************************************************************************/
+import javax.swing.JButton;
 
-public class Cell {
-  private Cell north;
-  private Cell east;
-  private Cell south;
-  private Cell west;
-  private Cell northEast;
-  private Cell northWest;
-  private Cell southEast;
-  private Cell southWest;
-  private String data;
 
-  /********************************************************************
-	*
-	* Method: Cell
-	*
-	* Description: Constructor for the Cell class
-	*
-	* @param  None
-	*
-	* @return None
-	*
-	********************************************************************/
+public class Cell extends JButton {
 
-  public Cell() {
-    north = null;
-    east = null;
-    south = null;
-    west = null;
-    northEast = null;
-    northWest = null;
-    southEast = null;
-    southWest = null;
-    data = null;
-  }
+	private static final long serialVersionUID = 1L;
+	private int data = 0;
+	private boolean mine = false;
+	private boolean check = false;
+	private int i;
+	private int j;
+	private boolean flagged = false;
 
-  /********************************************************************************
-	*
-	* Method: setNorth
-	*
-	* Description: Sets the north value of the current cell to a given cell
-	*
-	* @param  cell  The Cell which will be directly north of the current cell
-	*
-	* @return None
-	*
-	********************************************************************************/
+	public void increment(){
+		data++;
+	}
+	public boolean isFlagged(){
+		return flagged;
+	}
+	public void setFlag(boolean flag){
+		flagged = flag;
+	}
+	public boolean isMine(){
+		return mine;
+	}
+	public boolean isChecked(){
+		return check;
+	}
+	public boolean checkMine(){
+		if(mine == true){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	public int setMine(){
+		if(mine == false){
+			mine = true;
+			return 1;
+		}
+		return 0;
+	}
+	public void setId(int x, int y){
+		i = x;
+		j = y;
+	}
+	public int getI(){
+		return i;
+	}
+	public int getJ(){
+		return j;
+	}
+	public void reveal(){
+		if(mine == true){
+			this.setText("Mine");
+			check = true;
+			this.setEnabled(false);
+		}
+		else{
+			this.setText(String.valueOf(data));
+			check = true;
+			this.setEnabled(false);
+		}
+	}
+	public boolean isEmpty(){
+		return data == 0;
+	}
+	public boolean checkCell(){
+		if(data == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	public int getData(){
+		return data;
+	}
 
-  public void setNorth(Cell cell) {
-    this.north = cell;
-  }
-
-  /********************************************************************************
-	*
-	* Method: setEast
-	*
-	* Description: Sets the east value of the current cell to a given cell
-	*
-	* @param  cell  The Cell which will be directly east of the current cell
-	*
-	* @return None
-	*
-	********************************************************************************/
-
-  public void setEast(Cell cell) {
-    this.east = cell;
-  }
-
-  /********************************************************************************
-	*
-	* Method: setSouth
-	*
-	* Description: Sets the south value of the current cell to a given cell
-	*
-	* @param  cell  The Cell which will be directly south of the current cell
-	*
-	* @return None
-	*
-	********************************************************************************/
-
-  public void setSouth(Cell cell) {
-    this.south = cell;
-  }
-
-  /********************************************************************************
-	*
-	* Method: setWest
-	*
-	* Description: Sets the west value of the current cell to a given cell
-	*
-	* @param  cell  The Cell which will be directly west of the current cell
-	*
-	* @return None
-	*
-	********************************************************************************/
-
-  public void setWest(Cell cell) {
-    this.west = cell;
-  }
-
-  /********************************************************************************
-  *
-  * Method: setWest
-  *
-  * Description: Sets the west value of the current cell to a given cell
-  *
-  * @param  cell  The Cell which will be directly west of the current cell
-  *
-  * @return None
-  *
-  ********************************************************************************/
-
-  public void setNorthEast(Cell cell) {
-    this.northEast = cell;
-  }
-
-  /********************************************************************************
-  *
-  * Method: setWest
-  *
-  * Description: Sets the west value of the current cell to a given cell
-  *
-  * @param  cell  The Cell which will be directly west of the current cell
-  *
-  * @return None
-  *
-  ********************************************************************************/
-
-  public void setNorthWest(Cell cell) {
-    this.northWest = cell;
-  }
-
-  /********************************************************************************
-  *
-  * Method: setWest
-  *
-  * Description: Sets the west value of the current cell to a given cell
-  *
-  * @param  cell  The Cell which will be directly west of the current cell
-  *
-  * @return None
-  *
-  ********************************************************************************/
-
-  public void setSouthEast(Cell cell) {
-    this.southEast = cell;
-  }
-
-  /********************************************************************************
-  *
-  * Method: setWest
-  *
-  * Description: Sets the west value of the current cell to a given cell
-  *
-  * @param  cell  The Cell which will be directly west of the current cell
-  *
-  * @return None
-  *
-  ********************************************************************************/
-
-  public void setSouthWest(Cell cell) {
-    this.southWest = cell;
-  }
-
-  /********************************************************************************
-	*
-	* Method: setData
-	*
-	* Description: Sets the data of the current cell to the given data
-	*
-	* @param  newData  The data which the cell should contain
-	*
-	* @return None
-	*
-	********************************************************************************/
-
-  public void setData(String newData) {
-    this.data = newData;
-  }
-
-  /********************************************************************************
-	*
-	* Method: getNorth
-	*
-	* Description: Returns the cell directly north of the given cell
-	*
-	* @param  None
-	*
-	* @return The cell directly north of the current cell
-	*
-	********************************************************************************/
-
-  public Cell getNorth() {
-    return this.north;
-  }
-
-  /********************************************************************************
-	*
-	* Method: getEast
-	*
-	* Description: Returns the cell directly east of the given cell
-	*
-	* @param  None
-	*
-	* @return The cell directly east of the current cell
-	*
-	********************************************************************************/
-
-  public Cell getEast() {
-    return this.east;
-  }
-
-  /********************************************************************************
-	*
-	* Method: getSouth
-	*
-	* Description: Returns the cell directly south of the given cell
-	*
-	* @param  None
-	*
-	* @return The cell directly south of the current cell
-	*
-	********************************************************************************/
-
-  public Cell getSouth() {
-    return this.south;
-  }
-
-  /********************************************************************************
-	*
-	* Method: getWest
-	*
-	* Description: Returns the cell directly west of the given cell
-	*
-	* @param  None
-	*
-	* @return The cell directly west of the current cell
-	*
-	********************************************************************************/
-
-  public Cell getWest() {
-    return this.west;
-  }
-
-  /********************************************************************************
-  *
-  * Method: getWest
-  *
-  * Description: Returns the cell directly west of the given cell
-  *
-  * @param  None
-  *
-  * @return The cell directly west of the current cell
-  *
-  ********************************************************************************/
-
-  public Cell getNorthEast() {
-    return this.northEast;
-  }
-
-  /********************************************************************************
-  *
-  * Method: getWest
-  *
-  * Description: Returns the cell directly west of the given cell
-  *
-  * @param  None
-  *
-  * @return The cell directly west of the current cell
-  *
-  ********************************************************************************/
-
-  public Cell getNorthWest() {
-    return this.northWest;
-  }
-
-  /********************************************************************************
-  *
-  * Method: getWest
-  *
-  * Description: Returns the cell directly west of the given cell
-  *
-  * @param  None
-  *
-  * @return The cell directly west of the current cell
-  *
-  ********************************************************************************/
-
-  public Cell getSouthEast() {
-    return this.southEast;
-  }
-
-  /********************************************************************************
-  *
-  * Method: getWest
-  *
-  * Description: Returns the cell directly west of the given cell
-  *
-  * @param  None
-  *
-  * @return The cell directly west of the current cell
-  *
-  ********************************************************************************/
-
-  public Cell getSouthWest() {
-    return this.southWest;
-  }
-
-  /********************************************************************************
-	*
-	* Method: getData
-	*
-	* Description: Returns the data contained in the current cell
-	*
-	* @param  None
-	*
-	* @return The data contained in the current cell
-	*
-	********************************************************************************/
-
-  public String getData() {
-    return this.data;
-  }
 }
